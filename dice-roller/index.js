e52c6ec8ec33711ec222ada3f0a14faffa2724ed
Ninad-18 <204015860+Ninad-18@users.noreply.github.com>
2025-08-16 21:46:36 +0530

    Create index.js

---
+++ dice-roller/index.js
@@ -0,0 +1,14 @@
+function rollDice(){
+    const numOfDice = document.getElementById("numOfDice").value;
+    const diceResult = document.getElementById("diceResult");
+    const diceImages = document.getElementById("diceImages");
+    const values = [];
+    const images = [];
+    for(let i =0 ; i < numOfDice ; i++){
+        const value = Math.floor(Math.random() * 6) + 1;
+        values.push(value);
+        images.push(`<img src="images/${value}.png">`);
+    }
+    diceImages.innerHTML = images.join('');
+    diceResult.textContent = `dice: ${values.join(",")}`;
+}
