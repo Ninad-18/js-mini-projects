c1fbb61491b5c36e368946c7141eb11a59d839f2
Ninad-18 <204015860+Ninad-18@users.noreply.github.com>
2025-08-16 21:51:37 +0530

    Create index.js

---
+++ random-password-generator/index.js
@@ -0,0 +1,37 @@
+function generatePassword(length,includeLowerCase,includeUpperCase,includeNumbers,includeSymbols)
+{
+  const lowercaseChars = 'qwertyuioplkjhgfdsazxcvbnm';
+  const uppercaseChars = 'QWERTYUIOPLKJHGFDSAZXCVBNM';
+  const numberChars = "0123456789";
+  const symbolsChars = "!@#$%^&*-=+/|";
+  let allowedChars = "";
+  let password = "";
+
+  allowedChars += includeLowerCase ? lowercaseChars : "";
+  allowedChars += includeUpperCase ? uppercaseChars : "";
+  allowedChars += includeNumbers ?  numberChars : "";
+  allowedChars += includeSymbols ? symbolsChars : "";
+ 
+  if (length <= 0){
+    return `(password length must be atleast 1)`;
+  }
+  if(allowedChars.length === 0){
+    return `(At least 1 set of characters need to be selected)`;
+  }
+
+  for (let i = 0 ; i < length ; i++){
+    const randomIndex = Math.floor(Math.random() * allowedChars.length);
+    password += allowedChars[randomIndex];
+  }
+  return password;
+}
+
+const passwordLength = 12;
+const includeLowerCase = true;
+const includeNumbers  = true;
+const includeSymbols = true;
+const includeUpperCase = true;
+
+const password = generatePassword(passwordLength,includeLowerCase,includeUpperCase,includeNumbers,includeSymbols);
+
+console.log(`Generated Password: ${password}`)
